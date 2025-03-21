5b8f727ad2fbdd9e962dcd3481c33ebb068d797a
davidschwarz153 <schwarz.david100@gmail.com>
2025-03-21 09:44:45 +0100

    fixxed error

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,25 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/nav/Nav";
-import { AlertCircle } from "lucide-react";
 
 export default function Layout() {
   return (
-<<<<<<< Updated upstream
-    <>
-      <div className="pb-24">
-        <Outlet/>
-      </div>
-      <Nav/>
-    </>
-  )
-=======
     <div className="min-h-screen relative bg-[#0F172A]">
       {/* Content */}
-      <div className="relative z-10">
+      <div className="relative z-10 pb-24">
         <Outlet />
-        <Nav />
       </div>
+      <Nav />
     </div>
   );
->>>>>>> Stashed changes
 }
